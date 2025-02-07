1a11177e8094a297f7e1d7c79b2bcd57911e1911
MarS1r <128149302+MarS1r@users.noreply.github.com>
2025-02-07 10:59:02 +0200

    Initials exercise

--- Initials/Initials.cpp
+++ Initials/Initials.cpp
@@ -1,1 +1,18 @@
+// Exercise to print initials in block letter format
+
 #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "M   M  SSS" << endl;
+    cout << "MM MM S   S" << endl;
+    cout << "MM MM S" << endl;
+    cout << "M M M  SSS" << endl;
+    cout << "M   M     S" << endl;
+    cout << "M   M S   S" << endl;
+    cout << "M   M  SSS" << endl;
+
+    return 0;
+}
